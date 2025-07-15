7a93c2efc9ed4e10a7f9bb5c959c426ee8ee6488
Ilya Morozov <morozov1308@yandex.ru>
2025-07-15 23:45:39 +0300

    hw26_исправил orderform

--- core/forms.py
+++ core/forms.py
@@ -37,14 +37,14 @@ class OrderForm(forms.ModelForm):
             # "appointment_date": forms.SplitDateTimeWidget(attrs={"class": "form-control"}, date_attrs={'class':'datepicker', 'type':'date'}, time_attrs={'class':'timepicker', 'type':'time'},)
         }
 
-        def clean_services(self):
-            master = self.cleaned_data.get('master')
-            services = self.cleaned_data.get('services')
+    def clean_services(self):
+        master = self.cleaned_data.get('master')
+        services = self.cleaned_data.get('services')
 
-            if master and services:
-                master_services = master.services.all()
-                for service in services:
-                    if service not in master_services:
-                        raise ValidationError(f'Мастер {master} не предоставляет услугу "{service}".')
-            return services
+        if master and services:
+            master_services = master.services.all()
+            for service in services:
+                if service not in master_services:
+                    raise ValidationError(f'Мастер {master} не предоставляет услугу "{service}".')
+        return services
 
